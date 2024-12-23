61be9c00e11c17b72e73f3be1628a7875f4735dc
psychleDev <psychledev@gmail.com>
2024-12-23 14:54:48 -0500

    add Auth0 signup redirect

--- functions.php
+++ functions.php
@@ -1,13 +1,39 @@
 <?php
 // Enqueue styles and scripts
-function journal_theme_enqueue_styles() {
+function journal_theme_enqueue_styles()
+{
     wp_enqueue_style('journal-theme-styles', get_template_directory_uri() . '/styles.css');
 }
 add_action('wp_enqueue_scripts', 'journal_theme_enqueue_styles');
 
 // Register navigation menu
-function journal_theme_setup() {
+function journal_theme_setup()
+{
     register_nav_menu('primary', __('Primary Menu', 'journal-theme'));
 }
 add_action('after_setup_theme', 'journal_theme_setup');
+
+// Change the default WP email address
+function custom_wp_mail_from($original_email_address)
+{
+    return 'support@journal.ignite30.co'; // Replace with your preferred email
+}
+add_filter('wp_mail_from', 'custom_wp_mail_from');
+
+// Change the default WP email "From" name
+function custom_wp_mail_from_name($original_email_from)
+{
+    return 'Ignite 30'; // Replace with your preferred from name
+}
+add_filter('wp_mail_from_name', 'custom_wp_mail_from_name');
+
+add_filter('auth0_auth_url', function ($auth_url, $state) {
+    // Check if we're on the registration page
+    if (isset($_GET['action']) && $_GET['action'] === 'register') {
+        // Add screen_hint=signup to the Auth0 URL
+        $auth_url = add_query_arg('screen_hint', 'signup', $auth_url);
+    }
+    return $auth_url;
+}, 10, 2);
+
 ?>
--- header.php
+++ header.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
     <title><?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <?php wp_head(); ?>
 </head>
@@ -11,7 +16,8 @@
         <img src="<?php echo get_template_directory_uri(); ?>/header-image.jpg" alt="Journal Header">
     </div>
     <div class="container">
-         <header>
+       
+        <header>
          <div class="auth-button-container">
         <?php if (is_user_logged_in()): ?>
             <a href="<?php echo wp_logout_url(home_url()); ?>" class="auth-button logout">
